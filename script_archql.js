c505b7f199d1a3e1a14aed6de9451b908d4f208c
archql <59373768+archql@users.noreply.github.com>
2022-05-12 18:20:26 +0300

    Aq part finished

--- script_archql.js
+++ script_archql.js
@@ -1,51 +1,96 @@
-
-function create_window()
-{
-    let winDiv = document.createElement("div");
-    winDiv.className = "aq_modal";
-
-    let divContent = document.createElement("div");
-    divContent.className = "aq_modal_content";
-
-    let spanClose = createSpanText('close', '×');
-
-    let pReg = document.createElement("p");
-    pReg.appendChild(document.createTextNode("Registration"));
-
-    let divName = createInputBox('namebox', 'text', 'namebox', '', 'required', "Name");
-    let divEmail = createInputBox('emailbox', 'text', 'emailbox', '', 'required', "Email");
-    let divPhone = createInputBox('phonebox', 'text', 'phonebox', '', 'required', "Phone");
-    let divSend = createInputBox('sendbox', 'submit', 'modal-send', 'send', '', '');
-
-    divContent.append(spanClose, pReg, divName, divEmail, divPhone, divSend);
-    winDiv.append(divContent);
-    document.body.append(winDiv);
-    console.log(winDiv);
-    return winDiv;
+function next_random_int(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function show_window()
-{
-    console.log("Hello!!!");
+var is_capital = true;
+function make_str(length) {
+    var result           = '';
+    var capital_chars = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFFFFFFFFFFFFFFFFFFFFFFFGGGGGGGGGGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIJJJJKKKKKKKKLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPPPPPPPPPPPPPPPPQQQQQRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUVVVVVVVVVVVVWWWWWWWWWWWWWWWWWWWWXXXXYYYYYYYYYYYYYYYYYYYYZZ';
+    if (is_capital) {
+        result += capital_chars.charAt(Math.floor(Math.random() * capital_chars.length));
+        is_capital = false;
+    }
+
+    var characters       = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbccccccccccccccccccccccccccccccddddddddddddddddddddddddddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeefffffffffffffffffffffffffggggggggggggggggghhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiijjjjkkkkkkkkllllllllllllllllllllllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooppppppppppppppppqqqqqrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuvvvvvvvvvvvvwwwwwwwwwwwwwwwwwwwwxxxxyyyyyyyyyyyyyyyyyyyyzz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
     
-    modal.style.display = "block";
-
-    let timerID = setTimeout(close_window, 5000);
+    if (next_random_int(0, 15) < 2) {
+        result += '.';
+        is_capital = true;
+    }
+    
+    return result;
 }
-function close_window()
+
+function create_window(index, parent)
 {
-    modal.style.display = "none";
+    let name = parent.getElementsByTagName("h3").item(0).innerText;
+    let text = parent.getElementsByTagName("span").item(0).innerText;
+    
+    // window
+    let div_window = document.createElement("div");
+    div_window.className = "aq_modal";
+    div_window.id = "aq_modal_id_" + index.toString();
+
+    // content
+    let div_content = document.createElement("div");
+    div_content.className = "aq_modal_content";
+
+    // close baton setup
+    let span_close = createSpanText('aq_close', '×');
+
+    // content
+    let p_header = document.createElement("h4");
+    p_header.textContent = name;
+    
+    let p_text = document.createElement("div");
+    //for (i = 0; i < Math.floor(Math.random() * 5) + 5; i++)
+    //    p_text.textContent += text + " ";
+    is_capital = true;
+    for (i = 0; i < next_random_int(80, 180); i++)
+        p_text.textContent += make_str(next_random_int(3, 8)) + " ";
+
+    div_content.append(span_close, p_header, p_text);
+    div_window.append(div_content);
+    
+    document.body.append(div_window);
+    
+    // -- setup listeners --
+    // close on area click
+    div_window.onclick = function(event) {
+        if (event.target === div_window) {
+            closeWindow(div_window);
+        }
+    }
+    // close on cross click
+    span_close.addEventListener("click", () => {close_window(div_window); });
+    
+    return div_window;
+}
+
+function show_window(index)
+{
+    let window = document.getElementById("aq_modal_id_" + index.toString());
+    window.style.display = "block";
+}
+function close_window(window)
+{
+    window.style.display = "none";
 }
 
 console.log("Init!!!");
-create_window();
 
 var btns = document.getElementsByClassName("aq_table_button");
 for (let i = 0; i < btns.length; i++) {
-    //let parent = btns[i].parentElement;
+
+    let window = create_window(i, btns[i].parentElement);
 
     btns[i].addEventListener("click", () => {
-        show_window();
+        show_window(i);
     });
 };
 
